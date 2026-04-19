a8c81c030d17e33f83d254dd80b0999da4496cd9
PRATEEK RAJ <pr9424@srmist.edu.in>
2026-04-19 17:45:19 +0530

    Add files via upload
    Commit

---
+++ Unit1_Q1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Employee
+{
+private:
+    int id;
+    string name;
+    float basic, gross;
+
+public:
+    void read()
+    {
+        cout << "Enter ID: ";
+        cin >> id;
+        cout << "Enter Name: ";
+        cin >> name;
+        cout << "Enter Basic Salary: ";
+        cin >> basic;
+    }
+
+    void calc()
+    {
+        gross = basic + 0.2 * basic + 0.1 * basic;
+    }
+
+    void display()
+    {
+        cout << "\nID: " << id;
+        cout << "\nName: " << name;
+        cout << "\nGross Salary: " << gross << endl;
+    }
+};
+
+int main()
+{
+    Employee e1, e2;
+
+    cout << "Enter details for Employee 1\n";
+    e1.read();
+    e1.calc();
+
+    cout << "\nEnter details for Employee 2\n";
+    e2.read();
+    e2.calc();
+
+    cout << "\nEmployee Details:";
+    e1.display();
+    e2.display();
+
+    return 0;
+}
